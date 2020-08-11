fb30d45674de5583ebab5613c90b805f312d7735
alex-lt <alexandrchizhovl@gmail.com>
2020-08-12 03:36:29 +0500

    create: remove hover

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  
+  $(window).resize(function () {
+      if($(window).width() <= 768) {
+          $('.hero__button').removeClass('hero__button--hover');
+          $('.header__navbar-link').removeClass('header__navbar-link--hover');
+          $('.header__navbar-button').removeClass('header__navbar-button--hover');
+      };
+  });
+
+});
