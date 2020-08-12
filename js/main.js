293b48106a1b5ebd78329ec9606f6988ed4af044
alex-lt <alexandrchizhovl@gmail.com>
2020-08-12 19:41:57 +0500

    create: section - collection and media

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
           $('.hero__button').removeClass('hero__button--hover');
           $('.header__navbar-link').removeClass('header__navbar-link--hover');
           $('.header__navbar-button').removeClass('header__navbar-button--hover');
+          $('.collection-text__link').removeClass('collection-text__link--hover');
       };
   });
 
