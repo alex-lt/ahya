56bc0364e79ddddd0a65ae1584041ac50c28dc4d
alex-lt <alexandrchizhovl@gmail.com>
2020-08-16 15:49:03 +0500

    fix: hover effect on min-width 769px

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   
-  $(window).resize(function () {
-      if($(window).width() <= 768) {
-          $('.hero__button').removeClass('hero__button--hover');
-          $('.header__navbar-link').removeClass('header__navbar-link--hover');
-          $('.header__navbar-button').removeClass('header__navbar-button--hover');
-          $('.collection-text__link').removeClass('collection-text__link--hover');
-      };
-  });
+  // $(window).resize(function () {
+  //     if($(window).width() <= 768) {
+  //         $('.hero__button').removeClass('hero__button--hover');
+  //         $('.header__navbar-link').removeClass('header__navbar-link--hover');
+  //         $('.header__navbar-button').removeClass('header__navbar-button--hover');
+  //         $('.collection-text__link').removeClass('collection-text__link--hover');
+  //     };
+  // });
 
   // filtres
 
